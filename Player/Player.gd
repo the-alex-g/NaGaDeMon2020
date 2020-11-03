d55a1b697fbf5638c0c7034fe7f178c37f506ff0
Alex Gestwicki <alex.gestwicki@gmail.com>
2020-11-03 14:49:10 -0500

    Add better blue light

--- Player/Player.gd
+++ Player/Player.gd
@@ -1,12 +1,12 @@
 extends KinematicBody2D
 
-export var speed := 200.0
+export var speed := 300.0
 onready var _sprite := $Sprite
 onready var _light := $Light2D
 onready var _tween := $Tween
 var player_color:String = "red"
 var player_number:String = "1"
-var _light_colors := {"red":Color.red, "yellow":Color.yellow, "blue":Color.blue, "green":Color.green}
+var _light_colors := {"red":Color.red, "yellow":Color.yellow, "blue":Color(0,0.5,1,1), "green":Color.green}
 
 func _ready():
 	_light.color = _light_colors[player_color]
@@ -22,5 +22,5 @@ func _physics_process(delta):
 		velocity.x -= 1
 	if Input.is_action_pressed("right_"+player_number):
 		velocity.x += 1
-	var _error = move_and_collide(velocity*speed*delta)
+	var _error = move_and_collide(velocity.normalized()*delta*speed)
 	_sprite.rotation_degrees += 1
