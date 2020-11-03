d590ed7e9b994a83ed47f2ba54f47a3b471112ed
Alex Gestwicki <alex.gestwicki@gmail.com>
2020-11-03 13:35:16 -0500

    Add colors lock when selected

--- Main/Main.gd
+++ Main/Main.gd
@@ -1,21 +1,27 @@
 extends Node2D
 
 var _chosen_colors := []
-var _player1_color:String = "red"
-var _player2_color:String = "red"
-var _player3_color:String = "red"
-var _player4_color:String = "red"
-signal nope(id)
+var _player1_color:String
+var _player2_color:String
+var _player3_color:String
+var _player4_color:String
+signal color_chosen(color)
+signal color_taken_back(color)
 
 func _on_PlayerInitiator_chosen_color(color, id):
-	for chosen_color in _chosen_colors.size():
-		if color == _chosen_colors[chosen_color]:
-			if get("_player"+id+"_color") != color:
-				emit_signal("nope", id)
-			else:
-				_chosen_colors.remove(chosen_color)
-				_chosen_colors.append(color)
-				break
-		else:
-			_chosen_colors.append(color)
-			break
+	_set_player_color(color, id)
+	emit_signal("color_chosen", color)
+
+func _set_player_color(set_to, id):
+	if id == "1":
+		_player1_color = set_to
+	elif id == "2":
+		_player2_color = set_to
+	elif id == "3":
+		_player3_color = set_to
+	elif id == "4":
+		_player4_color = set_to
+
+func _on_PlayerInitiator_color_taken_back(color, id):
+	_set_player_color("", id)
+	emit_signal("color_taken_back", color)
--- Main/PlayerInitiator.gd
+++ Main/PlayerInitiator.gd
@@ -4,30 +4,32 @@ onready var _sprite := $Sprite
 export var player_id := "1"
 var player_colors := ["red", "yellow", "blue","green"]
 var _player_color_choice_id := 0
-var _player_color_choice := "red"
+export var _player_color_choice := "red"
+var _done := false
 signal chosen_color(color, id)
-signal player_ready(color, id)
-
-func _ready():
-	emit_signal("chosen_color", _player_color_choice, player_id)
+signal color_taken_back(color, id)
 
 func _process(_delta):
-	if Input.is_action_just_pressed("right_"+player_id):
-		_player_color_choice_id += 1
-		_player_color_choice = player_colors[_player_color_choice_id%4]
-		_sprite.play(_player_color_choice)
-		emit_signal("chosen_color", _player_color_choice, player_id)
-	if Input.is_action_just_pressed("left_"+player_id):
-		_player_color_choice_id -= 1
-		_player_color_choice = player_colors[_player_color_choice_id%4]
-		_sprite.play(_player_color_choice)
-		emit_signal("chosen_color", _player_color_choice, player_id)
-	if Input.is_action_just_pressed("select_"+player_id):
-		emit_signal("player_ready", _player_color_choice, player_id)
+	if not _done:
+		if Input.is_action_just_pressed("right_"+player_id):
+			_player_color_choice_id += 1
+			_player_color_choice = player_colors[_player_color_choice_id%(player_colors.size())]
+		if Input.is_action_just_pressed("left_"+player_id):
+			_player_color_choice_id -= 1
+			_player_color_choice = player_colors[_player_color_choice_id%(player_colors.size())]
+		if Input.is_action_just_pressed("select_"+player_id):
+			emit_signal("chosen_color", _player_color_choice, player_id)
+			_done = true
+	if Input.is_action_just_pressed("deselect_"+player_id):
+		_done = false
+		emit_signal("color_taken_back", _player_color_choice, player_id)
+	_sprite.play(_player_color_choice)
 
-func _on_Main_nope(id):
-	if id == player_id:
-		_player_color_choice_id += 1
-		_player_color_choice = player_colors[_player_color_choice_id%4]
-		_sprite.play(_player_color_choice)
-		emit_signal("chosen_color", _player_color_choice, player_id)
+func _on_Main_color_chosen(color):
+	for acolor in player_colors.size():
+		if player_colors[acolor] == color:
+			player_colors.remove(acolor)
+			break
+
+func _on_Main_color_taken_back(color):
+	player_colors.append(color)
