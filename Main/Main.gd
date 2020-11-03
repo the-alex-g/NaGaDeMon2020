6ef51fbe65db7330350d00632a917dc007f6b741
Alex Gestwicki <alex.gestwicki@gmail.com>
2020-11-03 14:11:49 -0500

    Add dynamic player selector generation

--- Main/Main.gd
+++ Main/Main.gd
@@ -1,19 +1,21 @@
 extends Node2D
 
 const player := preload("res://Player/Player.tscn")
+const selector := preload("res://Main/PlayerInitiator.tscn")
 onready var _main_menu := $MainMenu
 onready var _players := $Gameplay/Players
-var _chosen_colors := []
-var _player1_color:String = ""
-var _player2_color:String = ""
-var _player3_color:String = ""
-var _player4_color:String = ""
+onready var _selectors := $MainMenu/PlayerPickers
+var _player_count := 0
+var _player1_color:String = "not"
+var _player2_color:String = "not"
+var _player3_color:String = "not"
+var _player4_color:String = "not"
 signal color_chosen(color, id)
 signal color_taken_back(color)
 signal game_started
 
 func _process(_delta):
-	if _player1_color != "" and _player2_color != "":
+	if _player1_color != "" and _player2_color != "" and _player_count > 0:
 		if Input.is_action_just_pressed("start_game"):
 			_main_menu.hide()
 			emit_signal("game_started")
@@ -23,8 +25,35 @@ func _process(_delta):
 				Player.player_color = get("_player"+str(x+1)+"_color")
 				_players.add_child(Player)
 				print("SPAWNED ONE")
+	if Input.is_action_just_pressed("select_1") and _player1_color == "not":
+		_add_player_selector(1)
+		_player_count += 1
+		_player1_color = ""
+	if Input.is_action_just_pressed("select_2") and _player2_color == "not":
+		_add_player_selector(2)
+		_player_count += 1
+		_player2_color = ""
+	if Input.is_action_just_pressed("select_3") and _player3_color == "not":
+		_add_player_selector(3)
+		_player_count += 1
+		_player3_color = ""
+	if Input.is_action_just_pressed("select_4") and _player4_color == "not":
+		_add_player_selector(4)
+		_player_count += 1
+		_player4_color = ""
 
-func _on_PlayerInitiator_chosen_color(color, id):
+func _add_player_selector(id):
+	var Selector := selector.instance()
+	Selector.player_id = id
+	Selector.player_color_choice = Selector.player_colors[id-1]
+	var _error = Selector.connect("chosen_color", self, "_chosen_color")
+	var _error2 = Selector.connect("color_taken_back", self, "_color_taken_back")
+	var _error4 = connect("color_taken_back", Selector, "_color_taken_back")
+	var _error3 = connect("color_chosen", Selector, "_color_chosen")
+	Selector.position = Vector2(id*100, 300)
+	_selectors.add_child(Selector)
+
+func _chosen_color(color, id):
 	_set_player_color(color, id)
 	emit_signal("color_chosen", color, id)
 
@@ -38,6 +67,6 @@ func _set_player_color(set_to, id):
 	elif id == "4":
 		_player4_color = set_to
 
-func _on_PlayerInitiator_color_taken_back(color, id):
+func _color_taken_back(color, id):
 	_set_player_color("", id)
 	emit_signal("color_taken_back", color)
--- Main/PlayerInitiator.gd
+++ Main/PlayerInitiator.gd
@@ -3,8 +3,8 @@ extends Node2D
 onready var _sprite := $Sprite
 export var player_id := "1"
 var player_colors := ["red", "yellow", "blue","green"]
-var _player_color_choice_id := 0
-export var _player_color_choice := "red"
+var player_color_choice_id := 0
+export var player_color_choice := "red"
 var _done := false
 signal chosen_color(color, id)
 signal color_taken_back(color, id)
@@ -12,27 +12,27 @@ signal color_taken_back(color, id)
 func _process(_delta):
 	if not _done:
 		if Input.is_action_just_pressed("right_"+player_id):
-			_player_color_choice_id += 1
-			_player_color_choice = player_colors[_player_color_choice_id%(player_colors.size())]
+			player_color_choice_id += 1
+			player_color_choice = player_colors[player_color_choice_id%(player_colors.size())]
 		if Input.is_action_just_pressed("left_"+player_id):
-			_player_color_choice_id -= 1
-			_player_color_choice = player_colors[_player_color_choice_id%(player_colors.size())]
+			player_color_choice_id -= 1
+			player_color_choice = player_colors[player_color_choice_id%(player_colors.size())]
 		if Input.is_action_just_pressed("select_"+player_id):
-			emit_signal("chosen_color", _player_color_choice, player_id)
+			emit_signal("chosen_color", player_color_choice, player_id)
 			_done = true
 	if Input.is_action_just_pressed("deselect_"+player_id):
 		_done = false
-		emit_signal("color_taken_back", _player_color_choice, player_id)
-	_sprite.play(_player_color_choice)
+		emit_signal("color_taken_back", player_color_choice, player_id)
+	_sprite.play(player_color_choice)
 
-func _on_Main_color_chosen(color, id):
-	if color == _player_color_choice and player_id != id:
-		_player_color_choice_id += 1
-		_player_color_choice = player_colors[_player_color_choice_id%(player_colors.size())]
+func _color_chosen(color, id):
+	if color == player_color_choice and player_id != id:
+		player_color_choice_id += 1
+		player_color_choice = player_colors[player_color_choice_id%(player_colors.size())]
 	for acolor in player_colors.size():
 		if player_colors[acolor] == color:
 			player_colors.remove(acolor)
 			break
 
-func _on_Main_color_taken_back(color):
+func _color_taken_back(color):
 	player_colors.append(color)
